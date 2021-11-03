eed067a9118f15e4eae760922bf36638be43cd31
Serhat Tanrikut <serhat.tanrikut@gmail.com>
2021-11-03 23:04:31 +0100

    first commit

---
+++ src/wodogaming/game/games.ts
@@ -0,0 +1,8 @@
+export interface Game {
+    id:number;
+    name:string;
+    description:string;
+}
+
+export var agaio = {id:1, name:"Agario", description:"Open source agario"}, 
+snake = {id:2, name:"Snake", description:"Open source snake"};
