3142247d6eda02f681932f7df07504f9cfc9c0b1
Serhat Tanrikut <serhat.tanrikut@gmail.com>
2021-11-03 22:39:05 +0100

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,11 @@ import { RouterModule } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DemoModule } from './module/demo/demo.module';
+
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    DemoModule,
-    RouterModule.register([
-      {
-        path: 'api',
-        module: DemoModule
-      },
-    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
